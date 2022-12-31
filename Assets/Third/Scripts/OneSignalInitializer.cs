01cf89f290ace864cb9e6f85e6a39c8608109839
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-31 18:53:35 +0300

    update

--- Assets/Third/Scripts/OneSignalInitializer.cs
+++
@@ -1,7 +0,0 @@
-using OneSignalSDK;
-using UnityEngine;
-
-public class OneSignalInitializer : MonoBehaviour
-{
-    private void Start() => OneSignal.Default.Initialize("563bde23-68f2-4681-a793-39f9c81475a0");
-}
